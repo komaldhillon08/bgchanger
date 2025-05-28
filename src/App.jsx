7a2e2ec4144708e91a49fa3062a41cfc8161cd5c
komaldhillon08 <komaldhillonpb19@gmail.com>
2025-05-28 18:59:03 +0530

    click the button and background color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color , setColor] = useState("olive")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 gap-5">
+          <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-5 py-1 rounded-3xl cursor-pointer	" style={{backgroundColor: "red"}}>
+             <button onClick={ () => {setColor("red")} } className="outline-none px-4 py-1 rounded-full text-black shadoe-lg cursor-pointer">Red</button>    
+          </div>
+          <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-5 py-1 rounded-3xl cursor-pointer	" style={{backgroundColor: "Green"}}>
+             <button  onClick={ () => {setColor("Green")} } className="outline-none px-4 py-1 rounded-full text-black shadoe-lg cursor-pointer">Green</button>
+          </div>
+           <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-5 py-1 rounded-3xl cursor-pointer	" style={{backgroundColor: "yellow"}}>
+              <button  onClick={ () => {setColor("yellow")} } className="outline-none px-4 py-1 rounded-full text-black shadoe-lg cursor-pointer">yellow</button>
+
+          </div>
+           <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-5 py-1 rounded-3xl cursor-pointer	" style={{backgroundColor: "gray"}}>
+              <button  onClick={ () => {setColor("gray")} } className="outline-none px-4 py-1 rounded-full text-black shadoe-lg cursor-pointer">gray</button>
+          </div>
+        </div>
+     </div>
+   
     </>
   )
 }
